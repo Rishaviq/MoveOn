52a8e72f9aabdfea3fac0b741ffd10da1a928a5c
Rishaviq <juliqnbg@gmail.com>
2024-12-01 09:00:15 +0200

    progress bar

--- Models/HealthAppContext.cs
+++ Models/HealthAppContext.cs
@@ -34,7 +34,7 @@ public partial class HealthAppContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=health_app;user=root;password=Supermen30", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
+        => optionsBuilder.UseMySql(_configuration["SQL:connection"], Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
